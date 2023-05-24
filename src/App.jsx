4399b9aae87a030ff78d71510a70b6f727919c7a
Breno <onerbreno@outlook.com>
2023-05-23 23:54:47 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// JSX = Javascript + XML (HTML)
+import Post from "./Post"
+
+
+function App() {
+  return (
+    <div>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Post.jsx
@@ -0,0 +1,7 @@
+function Post() {
+    return <p>Post</p>
+}
+
+export default Post
+
+// Default Exports vs named Exports
