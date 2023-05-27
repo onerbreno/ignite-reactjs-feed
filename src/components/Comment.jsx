91195aef07695fea88a54a13fcca42482aa28ac5
Breno <onerbreno@outlook.com>
2023-05-27 00:36:11 -0300

    feat Add comment styling

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+import { Avatar } from './Avatar'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/onerbreno.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Breno Dias</strong>
+                            <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:11:30">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+
+                    </header>
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp size={20}/>
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
