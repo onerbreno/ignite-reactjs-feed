1454e3eaffb972201ff9c1b5e5d0493ac7e5e00c
Breno <onerbreno@outlook.com>
2023-05-27 00:29:33 -0300

    feat Add sidebar and post styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import styles from './Sidebar.module.css'
 
 import { PencilSimpleLine } from 'phosphor-react'
@@ -11,10 +12,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img 
-                    className={styles.avatar} 
-                    src="https://github.com/onerbreno.png" 
-                />
+                <Avatar src="https://github.com/onerbreno.png" />
 
                 <strong>Breno Dias</strong>
                 <span>Web Developer</span>
